c711072c1b04f8f62558cf6106f72adcc5c2b531
Carl-Rabbit <479843162@qq.com>
2020-12-21 17:52:18 +0800

    Update RecordInst (add modifiedRoleId)

--- src/main/java/com/ooad/xproject/dto/RecordInstDTO.java
+++ src/main/java/com/ooad/xproject/dto/RecordInstDTO.java
@@ -13,26 +13,13 @@ public class RecordInstDTO {
     private Timestamp createdTime;
     private Timestamp modifiedTime;
     private String derived;
-    private String tchName;
-    private String email;
     private String content;
     private String baseContent;
     private String comments;
 
-    private String derivedStr;
+    private String tchName;
+    private String email;
 
-    public void setDerived(String derived) {
-        this.derived = derived;
-        this.derivedStr = "";
-        if ("".equals(derived)) {
-            return;
-        }
-
-        for (String s : derived.split(",")) {
-            String[] ss = s.split("#");
-            this.derivedStr += ss[1] + " ";
-        }
-
-        this.derivedStr = this.derivedStr.substring(0, derivedStr.length() - 1);
-    }
+    private String modifiedTchName;
+    private String modifiedTchEmail;
 }
